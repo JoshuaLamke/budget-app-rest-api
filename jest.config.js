ee8eded4381784dbaa79e2a8d56c2b7c1011c9ab
Joshua Lamke <joshua.lamke@gmail.com>
2023-05-07 13:45:58 -0400

    unignore jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    }
+  },
+};
